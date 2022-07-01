4c501af54fcb48af74b427f8ef023b320d77b594
huseyinbatta <huseyinbattal42@gmail.com>
2022-07-01 19:03:05 +0300

    props

--- src/UserList.js
+++ src/UserList.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { UserContext } from "./context/UserContext";
 import User from "./User";
 
-function UserList({ changeColor, users }) {
+function UserList({ users }) {
   //console.log("USERLIST.JS PROPS::", changeColor, users);
   const contextData = useContext(UserContext);
   console.log("USER-LIST-CONTEXT-DATA::",contextData);
@@ -12,7 +12,7 @@ function UserList({ changeColor, users }) {
       UserList
       {/* <User changeColor={changeColor} user={users[0]} /> */}
       {users.map((user) => (
-        <User key={user.id} changeColor={changeColor} user={user} />
+        <User key={user.id} user={user} />
       ))}
     </div>
   );
